bf86b3fe53d99e62d42be22728ad6c3c02d10e51
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2020-02-05 21:16:43 +0900

    インデントの修正

--- src/status/Main.php
+++ src/status/Main.php
@@ -9,33 +9,31 @@ use pocketmine\event\Listener;
 
 use pocketmine\plugin\PluginBase;
 
-use onebone\economyapi\EconomyAPI;
+use onebone\economyapi\EconomyAPI;//API
 use Saisana299\easyscoreboardapi\EasyScoreboardAPI;
 
-use status\scoreTask\scoretask;
+use status\Task\scoreTask;
 
 class Main extends PluginBase implements Listener{
 
 	public function onEnable(){ 
 		date_default_timezone_set('Asia/Tokyo');
-
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
-        $this->getScheduler()->scheduleRepeatingTask(new scoretask($this), 5);
-
-		$this->getLogger()->info("StatusScoreBoardを読み込みました。by  yutarou1241477");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getScheduler()->scheduleRepeatingTask(new scoreTask($this), 5);
+			$this->getLogger()->info("StatusScoreBoardを読み込みました。by yutarou1241477");
 		$this->api = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-        if($this->api == null){
-		$this->getLogger()->error("EconomyAPIが見つかりません。サーバーを停止します。");
-		$this->getServer()->shutdown();
+		if($this->api == null){
+			$this->getLogger()->error("EconomyAPIが見つかりません。サーバーを停止します。");
+			$this->getServer()->shutdown();
 		}else{
-		$this->getLogger()->info("EconomyAPIを確認しました。");
+			$this->getLogger()->info("EconomyAPIを確認しました。");
 		}
 		$this->api = $this->getServer()->getPluginManager()->getPlugin("EasyScoreboardAPI");
-        if($this->api == null){
-		$this->getLogger()->error("EasyScoreboardAPIが見つかりません。サーバーを停止します。");
-		$this->getServer()->shutdown();
+		if($this->api == null){
+			$this->getLogger()->error("EasyScoreboardAPIが見つかりません。サーバーを停止します。");
+			$this->getServer()->shutdown();
 		}else{
-		$this->getLogger()->info("EasyScoreboardAPIを確認しました。");
+			$this->getLogger()->info("EasyScoreboardAPIを確認しました。");
 		}
     }
 }
